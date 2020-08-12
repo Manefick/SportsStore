097385e5e4dd1e5c52ef05353c3d13f1db8f6703
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-08-12 16:38:42 +0300

    Add administration page , add view with products and buttons edit add product and delete

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -15,6 +15,19 @@ namespace SportsStore.Controllers
             repository = repoService;
             cart = cartService;
         }
+        public ViewResult List() =>
+            View(repository.Orders.Where(o => !o.Shipped));
+        [HttpPost]
+        public IActionResult MarkShipped(int orderID)
+        {
+            Order order = repository.Orders.FirstOrDefault(p => p.OrderID == orderID);
+            if(order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
 
         public ViewResult Checkout() => View(new Order());
 
